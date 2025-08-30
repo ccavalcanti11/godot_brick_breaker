033a885e6f611c0cc0c8086734496fb84a9a0827
ccavalcanti <cgc.cavalcanti11@gmail.com>
2025-08-30 11:58:05 -0300

    feat: added check for purpleBrick in ball collider

--- brickbreaker/scripts/ball.gd
+++ brickbreaker/scripts/ball.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 	if collision:
 		var normal = collision.get_normal()
 		var collider = collision.get_collider()
-		
+		print("Ball collided with: ", collider.name)
 		# Check if the ball collided with the paddle
 		if collider.name == "Paddle":
 			#1. Factor in paddle momentum
@@ -22,6 +22,8 @@ func _physics_process(delta: float) -> void:
 			var paddle_width = collider.get_width() # Use the paddle's get_width() method
 			var offset = (global_position.x - collider.global_position.x) / (paddle_width / 2)
 			velocity.x += offset * speed # Affect trajectory based on hit location
-			
+		if collider.name == "PurpleBrick":
+			collider.call("hit")
+		
 		# Calculate bounce effect
 		velocity = velocity.bounce(normal).normalized() * speed
