20d8ea4e8368f5cb01da1096ed2f9de25ccf8867
ccavalcanti <cgc.cavalcanti11@gmail.com>
2025-09-04 23:02:56 -0300

    feat: balls velocity increases with time

--- brickbreaker/scripts/ball.gd
+++ brickbreaker/scripts/ball.gd
@@ -2,12 +2,18 @@ extends RigidBody2D
 
 @export var speed = 400
 @export var min_vertical_speed = 100 # min vertical velocity to avoid being too horizontal
+@export var speed_increment = 30 # How much to increase the speed per tick
+@export var max_speed = 800 # Maximum speed the ball can reach
+@export var speed_increase_interval = 5.0 # Time in seconds between speed increases
+
 var velocity = Vector2.ZERO
+var _time_since_last_increase = 0.0 # Tracks time since the last speed increase
 
 func launch(dir: Vector2):
 	velocity = dir.normalized() * speed
 
 func _physics_process(delta: float) -> void:
+	increase_speed(delta)
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		var normal = collision.get_normal()
@@ -35,3 +41,10 @@ func _physics_process(delta: float) -> void:
 		if abs(velocity.y) <= min_vertical_speed: # Check if y is too small
 			velocity.y = sign(velocity.y) * min_vertical_speed # Fix it to the threshold while keeping the direction
 	
+func increase_speed(delta: float) -> void:
+	_time_since_last_increase += delta
+	if _time_since_last_increase >= speed_increase_interval:
+		_time_since_last_increase = 0.0
+		if speed < max_speed:
+			speed += speed_increment
+			print("Increased ball speed to: ",  speed)
